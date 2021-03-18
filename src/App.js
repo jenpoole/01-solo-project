27141dc2cf61f8e4bc16f9ef0d60b49f36893df4
jenpoole <contact.vandepool@gmail.com>
2021-03-18 16:06:23 -0400

    add state to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Navigation from './components/Navigation/Navigation'
 import ImageUploadForm from './components/ImageUploadForm/ImageUploadForm'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      input: ''
+    }
+  }
+
   render () {
     return (
       <div className="App">

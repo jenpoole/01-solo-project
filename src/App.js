f3dfe8df7b23e7b85feaae76318ba073649d4cf5
jenpoole <contact.vandepool@gmail.com>
2021-03-18 15:44:07 -0400

    create navigation component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import { Component } from 'react';
+import Navigation from './components/Navigation/Navigation'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>Hello there!</h1>
+        <h1>Verified! Face Recognition App</h1>
         <Navigation></Navigation>
         {/* rename this to ImageUploadForm */}
-        <ImageLinkForm></ImageLinkForm>
-        <FaceRecognition></FaceRecognition>
+        {/* <ImageLinkForm></ImageLinkForm>
+        <FaceRecognition></FaceRecognition> */}
       </div>
     )
   }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+// import { Component } from 'react';
+
+// create a stateless component (pure function)
+const Navigation = () => {
+  return (
+    <nav>
+      <p>Sign Out</p>
+    </nav>
+  )
+}
+
+export default Navigation;

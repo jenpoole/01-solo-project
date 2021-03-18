d0f881d9b725213cfbafd7bbc832aa30f1f25769
jenpoole <contact.vandepool@gmail.com>
2021-03-18 17:11:33 -0400

    initialize Clarifai image detection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 import { Component } from 'react';
+import Clarifai from 'clarifai'
 import Navigation from './components/Navigation/Navigation'
 import ImageUploadForm from './components/ImageUploadForm/ImageUploadForm'
+
+// initialize Clarifai with API Key
+const clarifai = new Clarifai.App({
+  apiKey: '79c696e2305e4d63a65a6abdee053e52'
+})
+
+// important note for Clarifai images: use direct links to .jpg files
+const testImg = 'https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fblogs-images.forbes.com%2Fwomensmedia%2Ffiles%2F2018%2F07%2FPhoto-happy-1-unsplash-michael-dam.jpg'
 
 class App extends Component {
   constructor() {
@@ -16,10 +25,18 @@ class App extends Component {
     console.log(event.target.value)
   }
 
-  // handle image link submission
+  // handle image submission
   onSubmit = () => {
-    console.log('submitted!')
-  }
+    console.log('image submitted!')
+    
+    // face detection model
+    // note: 1st argument can either be `Clarifai.GENERAL_MODEL` or a string of numbers
+    clarifai.models.predict(Clarifai.GENERAL_MODEL, testImg)
+      .then(response => {
+        console.log(`clarifai response: ${JSON.stringify(response)}`) 
+      })
+      .catch();
+      }
 
   render () {
     return (
